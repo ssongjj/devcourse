caaf4def2e38f3e0ed9840aa95f61d25b3600e37
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2023-04-27 06:25:10 +0900

    polls_api serializer 완료한 부분까지 반영

--- mysite/polls_api/serializers.py
+++ mysite/polls_api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from polls.models import Question 
 
+'''
 class QuestionSerializer(serializers.Serializer):
     id  = serializers.IntegerField(read_only= True)
     question_text = serializers.CharField(max_length=200)
@@ -13,4 +14,8 @@ class QuestionSerializer(serializers.Serializer):
         instance.question_text = validated_data.get('question_text', instance.question_text) + '[시리얼라이저에서 업데이트]'
         instance.save()
         return instance
-
+'''
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id', 'question_text', 'pub_date']
--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,3 +1,112 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from polls.models import Question
+from polls_api.serializers import QuestionSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status, mixins, generics
+from rest_framework.views import APIView
 
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+'''
+class QuestionList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class QuestionDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+'''
+'''
+class QuestionList(APIView):
+    def get(self, request):
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class QuestionDetail(APIView):
+    def get(self, request, id):
+        question = get_object_or_404(Question, pk=id)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        question = get_object_or_404(Question, pk=id)
+        serializer = QuestionSerializer(question, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:    
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, id):
+        question = get_object_or_404(Question, pk=id)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+'''
 # Create your views here.
+@api_view(['GET', 'POST']) 
+def question_list(request):
+    if request.method == 'GET':
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many = True)
+        #serializer를 여러 개 줄 때는 many 옵션을 줘 serializer 해야 하는 게 여러 개임을 인식하게 해 준다.
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def question_detail(request, id):
+    question = get_object_or_404(Question, pk=id)
+    
+    if request.method == 'GET':
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = QuestionSerializer(question, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:    
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
